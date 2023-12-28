00f2aebe911c0495c4cab842326edb149e4111e5
jmryu <you@example.comthekrjm@naver.com>
2023-12-28 19:38:44 +0900

    weather app start

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+//첫페이지 위치기반 날씨
+//날씨 정보에 도시, 화씨 섭씨 날씨상태
+//5개의 버튼에 각각 도시배정
+//버튼 클릭 할 때마다 도시별 날씨상태가 나옴
+//현재 버튼을 누르면 현재위치의 도시 및 날씨정보가 나옴
+//로딩스피너
+
+function App() {
+  return <div className="App">hello</div>;
+}
+
+export default App;
